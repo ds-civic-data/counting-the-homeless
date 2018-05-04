7f6d163e4c5027af239113cb264d1d36e34f183b
Aaron Lawrence Till <aatill@rstudio-dev.reed.edu>
2018-05-04 10:51:28 -0700

    Tinkering with plots and shiny, including more affordable housing data

--- Shiny.R
+++ Shiny.R
@@ -7,7 +7,8 @@ library(fivethirtyeight)
 ##load data in plots.rmd
 select_year_total <- c("Total_Homeless_2017","Total_Homeless_2016","Total_Homeless_2015","Total_Homeless_2014","Total_Homeless_2013","Total_Homeless_2012","Total_Homeless_2011","Total_Homeless_2010","Total_Homeless_2009","Total_Homeless_2008","Total_Homeless_2007")
 select_year_change <- c('as.numeric(Change_2016)', 'as.numeric(Change_2015)', 'as.numeric(Change_2014)', 'as.numeric(Change_2013)', 'as.numeric(Change_2012)', 'as.numeric(Change_2011)', 'as.numeric(Change_2010)', 'as.numeric(Change_2009)', 'as.numeric(Change_2008)', 'as.numeric(Change_2007)')
-select_states <- c('Missouri', 'Oregon', 'California', 'Washington')
+select_states <- c('Missouri', 'Oregon', 'California', 'Washington', 'Kentucky', 'New York', 'Florida', 'Alaska')
+select_proportion_beds <- c('per100_2014', 'per100_2009')
 
 
 
@@ -20,10 +21,11 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      selectInput('year_total', 'Select Year for Oregon Counties', 
+      selectInput('year_total', 'Select Year PIT for Oregon Counties', 
                   choices = select_year_total),         
-      selectInput('year_change', 'Select Year for US States', choices = select_year_change),
-      selectInput('states', 'Select State(s)', choices = select_states, multiple = TRUE)
+      selectInput('year_change', 'Select Year PIT for US States', choices = select_year_change),
+      selectInput('states', 'Select State(s)', choices = select_states, multiple = TRUE),
+      selectInput('proportion_beds','Select Year of Affordable Housing Count', choices = select_proportion_beds)
       
     ),
     
@@ -39,7 +41,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$oregon_counties <- renderPlot({
-    PIT_Housing_Oregon_Merge %>% ggplot(aes_string(x='per100', y=input$year_total, color = 'Area')) + 
+    PIT_Housing_Oregon_Merge %>% ggplot(aes_string(x=input$proportion_beds, y=input$year_total, color = 'Area')) + 
       geom_point() +
       xlab('Number of Affordable Housing Units per 100 E.L.I. Individuals')
       
@@ -47,7 +49,7 @@ server <- function(input, output) {
   })
   
   output$usa_states <- renderPlot({
-    PIT_Housing_State_Merge %>% filter(State_Name == input$states) %>% ggplot(aes_string(x='per100', y=input$year_change, 
+    PIT_Housing_State_Merge %>% filter(State_Name == input$states) %>% ggplot(aes_string(x=input$proportion_beds, y=input$year_change, 
                                   color='State_Name')) + 
       geom_point() +
       xlab('Number of Affordable Housing Units per 100 E.L.I. Individuals') +
