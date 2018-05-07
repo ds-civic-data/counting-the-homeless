6e6fcc27bd3a39ee00aed8fd99dce043be39a168
Aaron Lawrence Till <aatill@rstudio-dev.reed.edu>
2018-05-07 15:52:40 -0700

    Fixed loads of bugs, got the shiny and plots updated for oregon data, built regression options into shiny

--- Shiny.R
+++ Shiny.R
@@ -5,11 +5,11 @@ library(tidyverse)
 library(fivethirtyeight)
 
 ##load data in plots.rmd
-select_year_total <- c("Total_Homeless_2017","Total_Homeless_2016","Total_Homeless_2015","Total_Homeless_2014","Total_Homeless_2013","Total_Homeless_2012","Total_Homeless_2011","Total_Homeless_2010","Total_Homeless_2009","Total_Homeless_2008","Total_Homeless_2007")
+select_year_total <- c("`Total Homeless, 2017`","`Total Homeless, 2016`","`Total Homeless, 2015`","`Total Homeless, 2014`","`Total Homeless, 2013`","`Total Homeless, 2012`","`Total Homeless, 2011`","`Total Homeless, 2010`","`Total Homeless, 2009`","`Total Homeless, 2008`","`Total Homeless, 2007`")
 select_year_change <- c('as.numeric(Change_2016)', 'as.numeric(Change_2015)', 'as.numeric(Change_2014)', 'as.numeric(Change_2013)', 'as.numeric(Change_2012)', 'as.numeric(Change_2011)', 'as.numeric(Change_2010)', 'as.numeric(Change_2009)', 'as.numeric(Change_2008)', 'as.numeric(Change_2007)')
-select_states <- c('Missouri', 'Oregon', 'California', 'Washington', 'Kentucky', 'New York', 'Florida', 'Alaska')
-select_proportion_beds <- c('per100_2014', 'per100_2009')
-
+select_states <- c('`Missouri`', '`Oregon`', '`California`', '`Washington`', '`Kentucky`', '`New York`', '`Florida`', '`Alaska`')
+select_x_variables <- c('`% Unemployed`', '`Gini Index`', '`Median Gross Rent`', '`Highschool Dropout Rate`', '`Total Population`', '`% In Poverty`', '`% Housing Units per ELI in 2014`', '`% Housing Units per Eli in 2009`')
+select_regression <- c('FALSE', 'loess', 'lm')
 
 
 # Define UI for application that plots 
@@ -25,13 +25,14 @@ ui <- fluidPage(
                   choices = select_year_total),         
       selectInput('year_change', 'Select Year for US States', choices = select_year_change),
       selectInput('states', 'Select State(s)', choices = select_states, multiple = TRUE),
-      selectInput('proportion_beds','Select X Variable', choices = select_proportion_beds)
+      selectInput('x_variable','Select X Variable', choices = select_x_variables),
+      selectInput('model', 'Fit Line', choices = select_regression)
       
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("oregon_counties"),
+      plotOutput("oregon_areas"),
       plotOutput("usa_states")
     )
   )
@@ -40,16 +41,15 @@ ui <- fluidPage(
 ##Server is where all of the computations happen
 server <- function(input, output) {
   
-  output$oregon_counties <- renderPlot({
-    PIT_Housing_Oregon_Merge %>% ggplot(aes_string(x=input$proportion_beds, y=input$year_total, color = 'Area')) + 
+  output$oregon_areas <- renderPlot({
+    Ultimate_Oregon %>% ggplot(aes_string(x=input$x_variable, y=input$year_total, color = 'Area')) + 
       geom_point() +
-      xlab('Number of Affordable Housing Units per 100 E.L.I. Individuals')
-      
-    
+      stat_smooth(method = input$model, se = FALSE, color = 'black') +
+      theme_tufte() 
   })
   
   output$usa_states <- renderPlot({
-    PIT_Housing_State_Merge %>% filter(State_Name == input$states) %>% ggplot(aes_string(x=input$proportion_beds, y=input$year_change, 
+    PIT_Housing_State_Merge %>% filter(State_Name == input$states) %>% ggplot(aes_string(x=input$x_variable, y=input$year_change, 
                                   color='State_Name')) + 
       geom_point() +
       xlab('Number of Affordable Housing Units per 100 E.L.I. Individuals') +
